fbc54f0579855f7266a23a8457d818a5cd175c4b
iamchw <iamchw@gmail.com>
2022-08-16 08:35:23 +0800

    Update third_party.sh

--- third_party.sh
+++ third_party.sh
@@ -9,8 +9,8 @@
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 sed -i '$a src-git jerrykuku https://github.com/jerrykuku/openwrt-package.git' feeds.conf.default
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
-echo 'src-git luci-app-tcpdump https://github.com/KFERMercer/luci-app-tcpdump' >>feeds.conf.default
-echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
+sed -i '$a src-git luci-app-tcpdump https://github.com/KFERMercer/luci-app-tcpdump' >>feeds.conf.default
+sed -i '$a src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
 # Use luci 18.06 on lienol feed
 #
 #
